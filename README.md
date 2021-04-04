2297fae0c2222a30e7e9260010c5fef7b8e9984a
Drew Wells <drew.wells00@gmail.com>
2021-04-04 13:51:11 -0500

    wallet query

---
+++ README.md
@@ -0,0 +1,13 @@
+Auth:
+    Login to opensea and copy x-api-key header from a domain request to a file named token
+
+    $ cat token
+    0556d29745754d448b4613d7a66d0535
+
+
+Usage:
+
+Pass a file with gql in it to get.sh. See the example(s) on how to call gql.
+
+
+    sh get.sh wallet.gql
---
+++ get.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+if [ ! -f $1 ] || [ $# -ne 1 ]
+then
+    echo Queries the github graphql API
+    echo "Usage:"
+    echo
+    echo "$0 somefile.gql"
+fi
+
+# read the gql query from the file named in the argument
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+TOKEN=$(cat $DIR/token)
+QUERY=$(jq -n \
+           --arg q "$(cat $1 | tr -d '\n')" \
+           '{ query: $q }')
+
+curl -v 'https://api.opensea.io/graphql/' \
+  -H 'authority: api.opensea.io' \
+  -H 'accept: */*' \
+  -H 'x-build-id: GBEenpG3YHeDfvEqWa7R0' \
+  -H 'x-viewer-address: 0x5f86b149b2f2e51911e3cd719f34eba439d2da5a' \
+  -H 'x-viewer-chain: ETHEREUM' \
+  -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36' \
+  -H "x-api-key: $TOKEN" \
+  -H 'content-type: application/json' \
+  -H 'sec-gpc: 1' \
+  -H 'origin: https://opensea.io' \
+  -H 'sec-fetch-site: same-site' \
+  -H 'sec-fetch-mode: cors' \
+  -H 'sec-fetch-dest: empty' \
+  -H 'referer: https://opensea.io/' \
+  -H 'accept-language: en-US,en;q=0.9' \
+  --data "$QUERY" \
+  --compressed
